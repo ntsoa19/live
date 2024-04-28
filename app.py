b50428f833f1ae06dcb866b0b9bd79a72320ca5a
ntsoa19 <131519432+ntsoa19@users.noreply.github.com>
2024-04-28 23:45:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,19 @@
 import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.memory import ConversationBufferMemory
-
 from deep_translator import GoogleTranslator
-# Initialize Google Generative AI
-
+from langchain_community.tools import DuckDuckGoSearchRun
 app = FastAPI()
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+search = DuckDuckGoSearchRun()
 
-# Initialize memory and tools
-memory = ConversationBufferMemory(memory_key="chat_history")
-tools = load_tools(["ddg-search"], llm=llm, description="add value on you answer",)
+class Query(BaseModel):
+    query: str
 
-# Define agent executor initialization function
-def initialize_agent_executor():
-    # Initialize agent executor
-    agent_executor = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
-    # Clear memory
-    memory.clear()
-    return agent_executor
-
-class Question(BaseModel):
-    fbid: str
-    question: str
-
-@app.post("/generate/")
-async def generate_response(question: Question):
-    try:
-        # Initialize agent executor and clear memory
-        agent_executor = initialize_agent_executor()
-        # Invoke agent with input from the request
-        response = agent_executor.invoke({"input": question.question})
-
-        return {"response_genaiput": response['output']}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
+@app.post("/search")
+async def search_query(query: Query):
+    result = search.run(query.query)
+    return {"result": result}
 
 class TranslationRequest(BaseModel):
   fbid: str
@@ -64,4 +38,4 @@ async def read_root():
     return {"message": "Hello, World!"}
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run(app, port=7000)
+  uvicorn.run(app, host="0.0.0.0", port=7000)
